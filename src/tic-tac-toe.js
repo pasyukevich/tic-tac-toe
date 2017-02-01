82c3866c127fffde19df51b4911e7e7eb0afbaa4
pasyukevich <pasyukevich@live.com>
2017-02-02 00:15:48 +0300

    Add files via upload

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,52 @@
 class TicTacToe {
     constructor() {
-
+        this.player="x";
+        this.winner=null;
+        this.location= [[],[],[]]
+        this.counter=9;
     }
 
-    getCurrentPlayerSymbol() {
-
-    }
+    getCurrentPlayerSymbol() {return this.player;}
 
     nextTurn(rowIndex, columnIndex) {
-
+        if(this.location[rowIndex][columnIndex]) return;
+        this.location[rowIndex][columnIndex]=this.player;
+        this.counter--;
+        if ((this.location[0][0]===this.location[1][1] && this.location[1][1]===this.location[2][2] && this.location[2][2]===this.player) ||
+            (this.location[2][0]===this.location[1][1] && this.location[1][1]===this.location[0][2] && this.location[0][2]===this.player) ||
+            (this.location[0][0]===this.location[1][0] && this.location[1][0]===this.location[2][0] && this.location[1][0]===this.player) ||
+            (this.location[0][1]===this.location[1][1] && this.location[1][1]===this.location[2][1] && this.location[1][1]===this.player) ||
+            (this.location[0][2]===this.location[1][2] && this.location[1][2]===this.location[2][2] && this.location[1][2]===this.player) ||
+            (this.location[0][0]===this.location[0][1] && this.location[0][1]===this.location[0][2] && this.location[0][1]===this.player) ||
+            (this.location[1][0]===this.location[1][1] && this.location[1][1]===this.location[1][2] && this.location[1][1]===this.player) ||
+            (this.location[2][0]===this.location[2][1] && this.location[2][1]===this.location[2][2] && this.location[2][1]===this.player)  )
+        {
+            this.winner=this.player;
+        }
+            if (this.player == "x")  this.player = "o";
+            else this.player = "x";
     }
 
     isFinished() {
-
+          if(this.counter===0 || this.winner) return true;
+          else return false;
     }
 
-    getWinner() {
-
-    }
+    getWinner() {return this.winner;}
 
     noMoreTurns() {
-
+                if (this.counter===0) return true;
+                return false;
     }
 
     isDraw() {
-
+        if (!this.counter && !this.winner) return true;
+        return false;
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        if (this.location[rowIndex][colIndex]==="x" || this.location[rowIndex][colIndex]==="o" ) return this.location[rowIndex][colIndex];
+        else return null;
     }
 }
-
 module.exports = TicTacToe;
